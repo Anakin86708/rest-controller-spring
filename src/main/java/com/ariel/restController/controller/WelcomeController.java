7161d30414541ca8431ca60899dd726ab995d737
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-17 13:37:35 -0300

    Comfiguração com type-safe.

--- src/main/java/com/ariel/restController/controller/WelcomeController.java
+++ src/main/java/com/ariel/restController/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.ariel.restController.controller;
 
+import com.ariel.restController.configuration.ConfigBean;
 import com.ariel.restController.services.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @Autowired
-    WelcomeService service;
+    private WelcomeService service;
+
+    @Autowired
+    private ConfigBean configBean;
 
     @GetMapping("/welcome")
     public String welcomeMessage() {
         return service.getWelcomeMessage();
+    }
+
+    @GetMapping("/dynamic-config")
+    public String dynamicConfig() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(configBean.getMessage());
+        builder.append("\n");
+        if (configBean.getValue()) {
+            builder.append(configBean.getNumber());
+        }
+        return builder.toString();
     }
 }
